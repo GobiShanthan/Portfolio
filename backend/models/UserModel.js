3aa1ed40381bdcd46c257e0c71657ce8368b943f
Gobinath Shanthakumar <gobishanthan04@gmail.com>
2021-05-31 22:45:39 -0400

    reset

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -3,14 +3,10 @@ import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
-    firstName:{
-      type:String,
-      required:true
-  },
-  lastName:{
-      type:String,
-      required:true
-  },
+    name: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
@@ -46,5 +42,34 @@ userSchema.pre("save",async function(next){
 
 
 
+
+
+
+
+
+
+
+
+
+/*
+userSchema.methods.matchPassword = async function(enteredPassword){
+  return await bcrypt.compare(enteredPassword,this.password)
+}
+
+userSchema.pre('save',async function(next){
+  if(!this.isModified('password')){
+    next() 
+  }
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password,salt)
+})
+
+
+
+
+*/
 const User = mongoose.model("User", userSchema);
+
+
+
 export default User;

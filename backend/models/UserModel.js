929648dc9ab567af56fac4aa6a78e14ee8167dd0
Gobinath Shanthakumar <gobishanthan04@gmail.com>
2021-05-31 22:51:10 -0400

    firs and lastname

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -7,6 +7,14 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    firstName:{
+      type:String,
+      required:true
+  },
+  lastName:{
+      type:String,
+      required:true
+  },
     email: {
       type: String,
       required: true,
@@ -40,34 +48,6 @@ userSchema.pre("save",async function(next){
   this.password = await bcrypt.hash(this.password,salt)
 })
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-userSchema.methods.matchPassword = async function(enteredPassword){
-  return await bcrypt.compare(enteredPassword,this.password)
-}
-
-userSchema.pre('save',async function(next){
-  if(!this.isModified('password')){
-    next() 
-  }
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password,salt)
-})
-
-
-
-
-*/
 const User = mongoose.model("User", userSchema);
 
 
